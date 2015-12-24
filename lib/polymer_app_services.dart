58b940946a3e945b92be09bbbc722e28cd4a5be3
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-12-24 15:14:57 +0100

    servicesManager

---
+++ lib/polymer_app_services.dart
@@ -0,0 +1,54 @@
+/**
+ * Created by lejard_h on 24/12/15.
+ */
+
+library polymer_app.services;
+
+import "polymer_app_manager.dart";
+import "utils.dart";
+import "dart:io";
+import "package:dart_style/dart_style.dart";
+
+class ServicesManager extends JsonObject {
+  DartFormatter _formatter = new DartFormatter();
+  String rootPath;
+  String appName;
+
+  String get completePath => "$rootPath/$path";
+
+  String get path => get("directory");
+  List get services => get("list");
+
+  ServicesManager.fromMap(Map config) : super.fromMap(config);
+  ServicesManager.fromJson(String json) : super.fromJson(json);
+
+  addToLibrary(String name) {
+    File services = new File("$completePath/services.dart");
+    services.writeAsString(services.readAsStringSync() +
+        "\n" +
+        "export '${toSnakeCase(name)}/${toSnakeCase(name)}.dart';\n");
+  }
+
+  createService(String name, [String content]) {
+    if (content == null) {
+      content = serviceDartTemplate(name);
+    }
+    createDartFile("$completePath/${toSnakeCase(name)}/${toSnakeCase(name)}.dart", content);
+    addToLibrary(name);
+  }
+
+  serviceDartTemplate(String name) => _formatter
+      .format('library ${toSnakeCase(appName)}.services.${toSnakeCase(name)};'
+          "import 'package:polymer_app/http_service.dart';"
+          "class ${toCamelCase(name)} {"
+          "static ${toCamelCase(name)} _cache;"
+          "HttpService http = new HttpService();"
+          "factory ${toCamelCase(name)}() {"
+          "if (_cache == null) {"
+          "_cache = new ${toCamelCase(name)}._internal();"
+          "}"
+          "return _cache;"
+          "}"
+          "${toCamelCase(name)}._internal();"
+          "}");
+}
