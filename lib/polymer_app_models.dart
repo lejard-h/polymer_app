352037f49abe8521ed6dbc265b4d895312480ef0
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-12-24 15:15:09 +0100

    models manager

---
+++ lib/polymer_app_models.dart
@@ -0,0 +1,53 @@
+/**
+ * Created by lejard_h on 24/12/15.
+ */
+
+library polymer_app.models;
+
+import "polymer_app_manager.dart";
+import "utils.dart";
+import "dart:io";
+import "package:dart_style/dart_style.dart";
+
+class ModelsManager extends JsonObject {
+  DartFormatter _formatter = new DartFormatter();
+  String rootPath;
+  String appName;
+
+  String get completePath => "$rootPath/$path";
+
+  String get path => get("directory");
+  List get models => get("list");
+  ModelsManager.fromMap(Map config) : super.fromMap(config);
+  ModelsManager.fromJson(String json) : super.fromJson(json);
+
+  addToLibrary(String name) {
+    File services = new File("$completePath/models.dart");
+    services.writeAsString(services.readAsStringSync() +
+        "\n" +
+        "export '${toSnakeCase(name)}/${toSnakeCase(name)}.dart';\n");
+  }
+
+  createModel(String name, [String content]) {
+    if (content == null) {
+      content = modelDartTemplate(name);
+    }
+    createDartFile(
+        "$completePath/${toSnakeCase(name)}/${toSnakeCase(name)}.dart",
+        content);
+    addToLibrary(name);
+  }
+
+  modelDartTemplate(String name) => _formatter
+      .format('library ${toSnakeCase(appName)}.services.${toSnakeCase(name)};'
+          'import "package:polymer_app/polymer_model.dart";'
+          'class ${toCamelCase(name)} extends PolymerModel {'
+          '@reflectable '
+          'String foo;'
+          '${toCamelCase(name)}(this.foo);'
+          '_fromJson(Map json) {'
+          'this.foo = json["json"];'
+          '}'
+          'Map get toMap => {"foo": foo};'
+          '}');
+}
